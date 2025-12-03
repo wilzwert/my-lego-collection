ef122313d0b06eea9d836b60d3db0bef69930ed0
Wilhelm Zwertvaegher <wilzwert@gmail.com>
2025-12-03 09:01:48 +0100

    fix(backend): auth module user created handler

--- back/src/Auth/Application/Handler/UserCreatedHandler.php
+++ back/src/Auth/Application/Handler/UserCreatedHandler.php
@@ -5,15 +5,19 @@ namespace App\Auth\Application\Handler;
 use App\Auth\Application\Command\GetIdentityQuery;
 use App\Auth\Domain\Event\IdentityCreatedEvent;
 use App\Auth\Domain\Model\Identity;
+use App\Auth\Domain\Repository\IdentityRepository;
 use App\Auth\Domain\Service\IdentityService;
+use App\Shared\Domain\Exception\EntityNotFoundException;
 use App\Shared\Domain\Model\EntityId;
 use App\Shared\Domain\Service\EventBus;
+use App\Shared\Domain\Service\TransactionProvider;
 use MyLegoCollection\SharedEvent\Event\UserCreatedIntegrationEvent;
 
 readonly class UserCreatedHandler
 {
     public function __construct(
-        private IdentityService $identityService,
+        private IdentityRepository  $identityRepository,
+        private TransactionProvider $transactionProvider,
         private EventBus $eventBus
 
     ) {
@@ -21,7 +25,21 @@ readonly class UserCreatedHandler
 
     public function __invoke(UserCreatedIntegrationEvent $event): void
     {
-        $identity = $this->identityService->completeIdentity(EntityId::fromString($event->getEntityId()));
-        $this->eventBus->dispatchAll($identity);
+        $identityId = EntityId::fromString($event->getEntityId());
+        $identity = $this->identityRepository->findById($identityId);
+        if (null === $identity) {
+            throw new EntityNotFoundException("Identity with id $identityId could not be found");
+        }
+
+        if ($identity->isComplete()) {
+            return;
+        }
+
+        $this->transactionProvider->transactional(function () use ($identity) {
+            $identity = $identity->complete();
+            $this->identityRepository->save($identity);
+            $this->eventBus->dispatchAll($identity);
+            return $identity;
+        });
     }
 }
